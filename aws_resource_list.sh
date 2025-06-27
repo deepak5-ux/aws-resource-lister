c651cee982824f800c1f97fb57b6e1b1433767f6
Deepak B <168259724+deepak5-ux@users.noreply.github.com>
2025-06-27 11:08:36 +0530

    Update aws_resource_list.sh

--- aws_resource_list.sh
+++ aws_resource_list.sh
@@ -33,15 +33,15 @@
 #
 #
 # check if  parameters are passed
- if [ $# -ne 2 ]; then
-	 echo "Usage: ./aws_resource_list.sh  <aws_region> <aws_service>"
- 	 echo "Example: ./aws_resource_list.sh us-east-1 ec2"
- 	 exit 1
- fi
+if [ $# -ne 2 ]; then
+	echo "Usage: ./aws_resource_list.sh  <aws_region> <aws_service>"
+ 	echo "Example: ./aws_resource_list.sh us-east-1 ec2"
+ 	exit 1
+fi
 
 # Assign the arguments to variables and convert the service to lowercase
-  aws_region=$1
-  aws_service=$2
+aws_region=$1
+aws_service=$2
 
 # Check if AWS CLI was installed
 if ! command -v aws &> /dev/null ; then
@@ -66,56 +66,56 @@ case $aws_service in
             aws rds describe-db-instances --region $aws_region
             ;;
         s3)
-        echo "Listing S3 Buckets in $aws_region"
-        aws s3api list-buckets --region $aws_region
-        ;;
-    cloudfront)
-        echo "Listing CloudFront Distributions in $aws_region"
-        aws cloudfront list-distributions --region $aws_region
-        ;;
-    vpc)
-        echo "Listing VPCs in $aws_region"
-        aws ec2 describe-vpcs --region $aws_region
-        ;;
-    iam)
-        echo "Listing IAM Users in $aws_region"
-        aws iam list-users --region $aws_region
-        ;;
-    route5)
-        echo "Listing Route53 Hosted Zones in $aws_region"
-        aws route53 list-hosted-zones --region $aws_region
-        ;;
-    cloudwatch)
-        echo "Listing CloudWatch Alarms in $aws_region"
-        aws cloudwatch describe-alarms --region $aws_region
-        ;;
-    cloudformation)
-        echo "Listing CloudFormation Stacks in $aws_region"
-        aws cloudformation describe-stacks --region $aws_region
-        ;;
-    lambda)
-        echo "Listing Lambda Functions in $aws_region"
-        aws lambda list-functions --region $aws_region
-        ;;
-    sns)
-        echo "Listing SNS Topics in $aws_region"
-        aws sns list-topics --region $aws_region
-        ;;
-    sqs)
-        echo "Listing SQS Queues in $aws_region"
-        aws sqs list-queues --region $aws_region
-        ;;
-    dynamodb)
-        echo "Listing DynamoDB Tables in $aws_region"
-        aws dynamodb list-tables --region $aws_region
-        ;;
-    ebs)
-        echo "Listing EBS Volumes in $aws_region"
-        aws ec2 describe-volumes --region $aws_region
-        ;;
-    *)
-        echo "Invalid service. Please enter a valid service."
-        exit 1
-        ;;
+            echo "Listing S3 Buckets in $aws_region"
+            aws s3api list-buckets --region $aws_region
+            ;;
+        cloudfront)
+	    echo "Listing CloudFront Distributions in $aws_region"
+	    aws cloudfront list-distributions --region $aws_region
+	    ;;
+        vpc)
+            echo "Listing VPCs in $aws_region"
+            aws ec2 describe-vpcs --region $aws_region
+            ;;
+        iam)
+            echo "Listing IAM Users in $aws_region"
+            aws iam list-users --region $aws_region
+            ;;
+        route5)
+            echo "Listing Route53 Hosted Zones in $aws_region"
+            aws route53 list-hosted-zones --region $aws_region
+            ;;
+        cloudwatch)
+            echo "Listing CloudWatch Alarms in $aws_region"
+            aws cloudwatch describe-alarms --region $aws_region
+            ;;
+        cloudformation)
+            echo "Listing CloudFormation Stacks in $aws_region"
+            aws cloudformation describe-stacks --region $aws_region
+            ;;
+        lambda)
+            echo "Listing Lambda Functions in $aws_region"
+            aws lambda list-functions --region $aws_region
+            ;;
+        sns)
+            echo "Listing SNS Topics in $aws_region"
+            aws sns list-topics --region $aws_region
+            ;;
+        sqs)
+            echo "Listing SQS Queues in $aws_region"
+            aws sqs list-queues --region $aws_region
+            ;; 
+        dynamodb)
+            echo "Listing DynamoDB Tables in $aws_region"
+            aws dynamodb list-tables --region $aws_region
+            ;;
+        ebs)
+            echo "Listing EBS Volumes in $aws_region"
+            aws ec2 describe-volumes --region $aws_region
+            ;;
+        *)
+            echo "Invalid service. Please enter a valid service."
+            exit 1
+            ;;
 esac
 
